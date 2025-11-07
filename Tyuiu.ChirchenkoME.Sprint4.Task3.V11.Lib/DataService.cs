37041652eec1c1078c6e8929ce8f5b392f06b15e
dimka <dimka556.dim4@gmail.com>
2025-11-07 16:53:45 +0500

    Task3

---
+++ Tyuiu.ChirchenkoME.Sprint4.Task3.V11.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.ChirchenkoME.Sprint4.Task3.V11.Lib
+{
+    public class DataService : ISprint4Task3V11
+    {
+        public int Calculate(int[,] array)
+        {
+            // Дан двумерный целочисленный массив 5 на 5 элементов, заполненный статическими значениями в диапазоне от 4 до 9. Подсчитайте количество нечетных элементов во всем массиве. 8, 6, 9, 4, 5, 9, 4, 8, 5, 6, 9, 7, 9, 8, 4, 4, 6, 5, 7, 8, 6, 6, 7, 6, 4,
+            int count = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] % 2 == 1)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.ChirchenkoME.Sprint4.Task3.V11/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using Tyuiu.ChirchenkoME.Sprint4.Task3.V11.Lib;
+
+namespace Tyuiu.ChirchenkoME.Sprint4.Task3.V11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int len;
+            Console.WriteLine("Введите размерность массива (5):");
+            len = Convert.ToInt32(Console.ReadLine());
+            int[,] numsArray = new int[len, len];
+            for (int i = 0; i < len; i++)
+            {
+                for (int j = 0; j < len; j++)
+                {
+                    Console.WriteLine($"Введите элемент массива [{i + 1},{j + 1}] (от 4 до 9):");
+                    numsArray[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("Исходный массив");
+            for (int i = 0; i < len; i++)
+            {
+                for (int j = 0; j < len; j++)
+                {
+                    Console.Write(numsArray[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            int result = ds.Calculate(numsArray);
+            Console.WriteLine("количество нечетных элементов во всем массиве:");
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+
+        }
+    }
+}
