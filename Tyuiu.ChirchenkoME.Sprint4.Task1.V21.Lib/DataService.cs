069c19d39adcf7150cf3b7926a389af722db0b63
dimka <dimka556.dim4@gmail.com>
2025-11-07 16:30:31 +0500

    Task1

---
+++ Tyuiu.ChirchenkoME.Sprint4.Task1.V21.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.ChirchenkoME.Sprint4.Task1.V21.Lib
+{
+    public class DataService : ISprint4Task1V21
+    {
+        public int Calculate(int[] array)
+        {
+            // Дан одномерный целочисленный массив на 11 элементов заполненный значениями с клавиатуры в диапазоне от 1 до 8 подсчитать произведение четных элементов массива. С клавиатуры: 2, 4, 8, 5, 2, 6, 5, 8, 8, 6, 6
+            int product = 1;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    product *= array[i];
+                }
+            }
+            return product;
+        }
+    }
+}
---
+++ Tyuiu.ChirchenkoME.Sprint4.Task1.V21/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using Tyuiu.ChirchenkoME.Sprint4.Task1.V21.Lib;
+
+namespace Tyuiu.ChirchenkoME.Sprint4.Task0.V26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int len;
+            Console.WriteLine("Введите длину массива (11):");
+            len = Convert.ToInt32(Console.ReadLine());
+
+            int[] numsArray = new int[len];
+
+            for (int i = 0; i < len; i++)
+            {
+                Console.WriteLine($"Введите элемент массива {i + 1} (от 1 до 8):");
+                numsArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Исходный массив");
+            for (int i = 0; i < len; i++)
+            {
+                Console.WriteLine(numsArray[i]);
+            }
+
+            int result = ds.Calculate(numsArray);
+
+            Console.WriteLine("Сумма четных элементов массива:");
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+
+        }
+    }
+}
